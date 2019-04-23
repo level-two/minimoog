ab433c9d6f3d10c5f666daaf0219f8e99b0db86c
Yauheni Lychkouski <elychkouski@gmail.com>
2019-04-23 22:56:41 +0300

    Added Pod: RxSwift

--- minimoog/Pods/Target Support Files/Pods-Minimoog/Pods-Minimoog-frameworks.sh
+++ minimoog/Pods/Target Support Files/Pods-Minimoog/Pods-Minimoog-frameworks.sh
@@ -155,11 +155,13 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CustomUiKit/CustomUiKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Grids/Grids.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CustomUiKit/CustomUiKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Grids/Grids.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
