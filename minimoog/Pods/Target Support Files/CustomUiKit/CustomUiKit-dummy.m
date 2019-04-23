4fd9dcd2b99e89d9e52a23ea35ec5ae2c19a56ab
Yauheni Lychkouski <elychkouski@gmail.com>
2019-04-23 10:20:58 +0300

    Pods: SnapKit, Grids

---
+++ minimoog/Pods/Target Support Files/CustomUiKit/CustomUiKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CustomUiKit : NSObject
+@end
+@implementation PodsDummy_CustomUiKit
+@end
---
+++ minimoog/Pods/Target Support Files/Pods-MinimoogAU/Pods-MinimoogAU-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MinimoogAU : NSObject
+@end
+@implementation PodsDummy_Pods_MinimoogAU
+@end
